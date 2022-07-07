def6f9fa05a8d5a4f5dbdfab251f9d7bf99805fd
Jeffrey <jeffrey.shively@hashicorp.com>
2022-07-07 12:40:59 -0400

    MVP before refactor

--- main.py
+++ main.py
@@ -41,39 +41,44 @@ def api_request(slug, headers=TFCB_HEADERS, method="GET"):
         print(e)
 
 
-def retrieve_workspaces():
+def retrieve_drift_status():
     slug = TFCB_SLUGS['list_workspaces']
     response = api_request(slug)
-    drift_list_dict = []
+    drift_payload = []
     for workspace in response['data']:
         drift_status_url = ""
-        drift_status = ""
+        drift_detected = ""
         drift_ran_date = ""
         workspace_name = workspace['attributes']['name']
         if drift_configured := workspace['attributes']['drift-detection']:
             drift_status_url = workspace['relationships']['current-assessment-result']['links']['related']
             drift_response = api_request(drift_status_url)
-            drift_status = drift_response['data']['attributes']['drifted']
+            drift_detected = drift_response['data']['attributes']['drifted']
             drift_ran_date = drift_response['data']['attributes']['created-at'].split(".")[0]
-        drift_list_dict.append({
+        drift_payload.append({
             "workspace_name": workspace_name,
             "drift_configured": drift_configured,
-            "drift_status": drift_status,
+            "drift_detected": drift_detected,
             "drift_last_checked": drift_ran_date
         })
-    return drift_list_dict
+    return drift_payload
 
 
-def create_csv(drift_list_dict):
+def create_csv(drift_payload):
     date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
-    keys = drift_list_dict[0].keys()
-    filename = f"{OUTPUT_DIR}/drift_{date}"
+    keys = drift_payload[0].keys()
+    filename = f"{OUTPUT_DIR}/drift_{date}.csv"
     with open(filename, "w") as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
-        dict_writer.writerows(drift_list_dict)
+        dict_writer.writerows(drift_payload)
+    print(f"Wrote output file: {filename}")
 
 
 if __name__ == "__main__":
-    drift_list_dict = retrieve_workspaces()
-    create_csv(drift_list_dict)
+    # Check if output directory exists and if not create it
+    if not os.path.isdir(OUTPUT_DIR):
+        print("Output directory does not exist. Creating output directory")
+        os.makedirs(OUTPUT_DIR)
+    drift_payload = retrieve_drift_status()
+    create_csv(drift_payload)
