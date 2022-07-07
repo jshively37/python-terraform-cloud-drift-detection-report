e71e206351d09d0bc94d3522b6952f82e1c9351d
Jeffrey <jeffrey.shively@hashicorp.com>
2022-07-07 12:30:19 -0400

    Functional MVP

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import csv
 import os
+from datetime import datetime
 from urllib.error import HTTPError
 
 # Ensure requests is installed
@@ -14,20 +16,22 @@ try:
     TFCB_ORG = os.environ.get("TFCB_ORG")
     TFCB_API_KEY = os.environ.get("TFCB_API_KEY")
     TFCB_PATH = os.environ.get("TFCB_PATH")
+    OUTPUT_DIR = os.environ.get("OUTPUT_DIR")
 except ImportError:
     print("python-dotenv not found")
 
+# API Endpoints
 TFCB_SLUGS = {
-    "list_orgs": "/api/v2/organizations",
     "list_workspaces": f"/api/v2/organizations/{TFCB_ORG}/workspaces"
 }
 
+# REST API Header Information
 TFCB_HEADERS = {
     "Authorization": f"Bearer {TFCB_API_KEY}"
 }
 
 
-def make_request(slug, headers=TFCB_HEADERS, method="GET"):
+def api_request(slug, headers=TFCB_HEADERS, method="GET"):
     url = TFCB_PATH + slug
     response = requests.request(method, url, headers=headers)
     try:
@@ -38,11 +42,9 @@ def make_request(slug, headers=TFCB_HEADERS, method="GET"):
 
 
 def retrieve_workspaces():
-    """_summary_
-    """
-    # Create the URL
     slug = TFCB_SLUGS['list_workspaces']
-    response = make_request(slug)
+    response = api_request(slug)
+    drift_list_dict = []
     for workspace in response['data']:
         drift_status_url = ""
         drift_status = ""
@@ -50,29 +52,28 @@ def retrieve_workspaces():
         workspace_name = workspace['attributes']['name']
         if drift_configured := workspace['attributes']['drift-detection']:
             drift_status_url = workspace['relationships']['current-assessment-result']['links']['related']
-            drift_response = make_request(drift_status_url)
-            drift_status = drift_detected(drift_response)
-            drift_ran_date = drift_last_ran(drift_response)
-        print(workspace_name, drift_status, drift_ran_date)
+            drift_response = api_request(drift_status_url)
+            drift_status = drift_response['data']['attributes']['drifted']
+            drift_ran_date = drift_response['data']['attributes']['created-at'].split(".")[0]
+        drift_list_dict.append({
+            "workspace_name": workspace_name,
+            "drift_configured": drift_configured,
+            "drift_status": drift_status,
+            "drift_last_checked": drift_ran_date
+        })
+    return drift_list_dict
 
 
-def drift_detected(drift_response):
-    """_summary_
-    """
-    return drift_response['data']['attributes']['drifted']
-
-
-def drift_last_ran(drift_response):
-    """_summary_
-
-    Args:
-        drift_response (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    return drift_response['data']['attributes']['created-at']
+def create_csv(drift_list_dict):
+    date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
+    keys = drift_list_dict[0].keys()
+    filename = f"{OUTPUT_DIR}/drift_{date}"
+    with open(filename, "w") as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(drift_list_dict)
 
 
 if __name__ == "__main__":
-    retrieve_workspaces()
+    drift_list_dict = retrieve_workspaces()
+    create_csv(drift_list_dict)
